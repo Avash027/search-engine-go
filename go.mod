72e69fd725c08fbc063428bef2d7297b3dcb0dc3
Avash027 <avashmitra2706@gmail.com>
2024-01-23 15:28:11 +0530

    added a simple web serve

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Avash027/search-engine-go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("query")
+
+	_, err := fmt.Fprintf(w, "You queried: %s", query)
+	if err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", queryHandler)
+
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
